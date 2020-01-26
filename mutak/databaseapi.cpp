58bd0253bb9e3fed979c62bf66f7c1cbfdcb323d
ahmed-debbech <debbech.ahmed@gmail.com>
2020-01-26 17:21:34 +0100

    Add working directory for each user after login

--- mutak/databaseapi.cpp
+++ mutak/databaseapi.cpp
@@ -5,11 +5,12 @@ DatabaseAPI::DatabaseAPI(){
 
 }
 void DatabaseAPI :: prepareUserDir(QString id){
-    QString ss(QStandardPaths::DesktopLocation);
-    std::cout << ss.toStdString() << std::endl;
-
-    userDir.setPath(ss);
+    QString s = QStandardPaths::locate(QStandardPaths::AppDataLocation,
+                                       QString(), QStandardPaths::LocateDirectory);
+    userDir.setPath(s);
     if(userDir.exists() == true){
-        std::cout << ss.toStdString() << std::endl;
+        if(userDir.cd(id)== false){
+            userDir.mkdir(id);
+        }
     }
 }
