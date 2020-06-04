7ec93555916a8a10df751ca709e5951ef1b4ef3f
ahmed-debbech <debbech.ahmed@gmail.com>
2020-06-04 23:59:45 +0100

    Add two options in search

--- mutak/search.cpp
+++ mutak/search.cpp
@@ -11,5 +11,22 @@ void MainWindow::on_cancel_search_clicked(){
     ui->menuBar1->setHidden(false);
 }
 void MainWindow::refreshSearch(){
-
+    int index = ui->searchBy->currentIndex();
+    vector <Track> t;
+    if(index == 0){
+        //search by song name
+            for(int i=0; i<=tracks.size()-1; i++){
+                if(ui->search_text->text().startsWith(tracks[i].getName()) == true){
+                    t.push_back(tracks[i]);
+                }
+            }
+    }else{
+        //search by artist name
+        for(int i=0; i<=tracks.size()-1; i++){
+            if(tracks[i].getArtist() == ui->search_text->text()){
+                t.push_back(tracks[i]);
+            }
+        }
+    }
+    this->addToList(t);
 }
